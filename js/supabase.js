8f0bb28bbf1bf8a363f1c7fd3d0b5bc1a307ea33
nobody-miyabi-suki <arthurmorganrx580@gmail.com>
2026-08-20 11:23:35 +0330

    Connect Supabase backend

---
+++ js/supabase.js
@@ -0,0 +1,17 @@
+const SUPABASE_URL = "https://cszwzywexcfczphjfffz.supabase.co";
+const SUPABASE_KEY = "sb_publishable_c9_F0IXyQWG48_tzu_iP9w_peSwZzIL";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+async function testSupabase() {
+    const { data, error } = await supabaseClient
+        .from("profiles")
+        .select("*");
+
+    console.log("Supabase test:", { data, error });
+}
+
+testSupabase();
